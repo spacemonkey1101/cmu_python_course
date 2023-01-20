f9505a300a5e46716d9c9e281a20a999ab2c98c7
spacemonkey1101 <ritambh54@gmail.com>
2023-01-20 18:27:28 +0530

    alternating sum

---
+++ hw4/hw4.py
@@ -0,0 +1,363 @@
+#################################################
+# hw4.py
+# name:
+# andrew id:
+#################################################
+
+import cs112_f22_week4_linter
+import math, copy
+
+#################################################
+# Helper functions
+#################################################
+
+def almostEqual(d1, d2, epsilon=10**-7): #helper-fn
+    # note: use math.isclose() outside 15-112 with Python version 3.5 or later
+    return (abs(d2 - d1) < epsilon)
+
+import decimal
+def roundHalfUp(d): #helper-fn
+    # Round to nearest with ties going away from zero.
+    rounding = decimal.ROUND_HALF_UP
+    # See other rounding options here:
+    # https://docs.python.org/3/library/decimal.html#rounding-modes
+    return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
+
+def rgbString(red, green, blue):
+     return f'#{red:02x}{green:02x}{blue:02x}'
+
+#################################################
+# Functions for you to write
+#################################################
+
+def alternatingSum(L):
+    if len(L) == 0:
+        return 0
+    sign = 1
+    sum = 0
+    for i in L:
+        sum += i*sign
+        sign *=-1
+    return sum
+
+def median(L):
+    return 42
+
+def smallestDifference(L):
+    if len(L) < 2:
+        return -1
+    diff = -1
+    for i in range(len(L)):
+        
+        for j in range(i+1,len(L)):
+        
+            if diff == -1 or diff > abs(L[i]-L[j]):
+                diff = abs(L[i]-L[j])
+
+    return diff
+
+def nondestructiveRemoveRepeats(L):
+    for i in range(len(L)):
+        
+        for j in range(i+1,len(L)-1):
+            if L[i] == L[j]:
+                L.pop(j)
+    return L
+
+def destructiveRemoveRepeats(L):
+    return 42
+
+def isSorted(L):
+    return 42
+
+def runLengthEncode(dataList):
+    return 42
+
+def runLengthDecode(rleList):
+    return 42
+
+def multiplyPolynomials(p1, p2):
+    return 42
+
+def bestScrabbleScore(dictionary, letterScores, hand):
+    return 42
+
+#################################################
+# Bonus/Optional
+#################################################
+
+def linearRegression(pointsList):
+    return 42
+
+def runSimpleProgram(program, args):
+    return 42
+
+#################################################
+# Test Functions
+#################################################
+
+def _verifyAlternatingSumIsNondestructive():
+    a = [1,2,3]
+    b = copy.copy(a)
+    # ignore result, just checking for destructiveness here
+    alternatingSum(a)
+    return (a == b)
+
+def testAlternatingSum():
+    print('Testing alternatingSum()...', end='')
+    assert(_verifyAlternatingSumIsNondestructive())
+    assert(alternatingSum([ ]) == 0)
+    assert(alternatingSum([1]) == 1)
+    assert(alternatingSum([1, 5]) == 1-5)
+    assert(alternatingSum([1, 5, 17]) == 1-5+17)
+    assert(alternatingSum([1, 5, 17, 4]) == 1-5+17-4)
+    print('Passed!')
+
+def _verifyMedianIsNondestructive():
+    a = [1,2,3]
+    b = copy.copy(a)
+    # ignore result, just checking for destructiveness here
+    median(a)
+    return (a == b)
+
+def testMedian():
+    print('Testing median()...', end='')
+    assert(_verifyMedianIsNondestructive())
+    assert(median([ ]) == None)
+    assert(median([ 42 ]) == 42)
+    assert(almostEqual(median([ 1 ]), 1))
+    assert(almostEqual(median([ 1, 2]), 1.5))
+    assert(almostEqual(median([ 2, 3, 2, 4, 2]), 2))
+    assert(almostEqual(median([ 2, 3, 2, 4, 2, 3]), 2.5))
+    assert(almostEqual(median([-2, -4, 0, -6]), -3))
+    assert(almostEqual(median([1.5, 2.5]), 2.0))
+    # now make sure this is non-destructive
+    a = [ 2, 3, 2, 4, 2, 3]
+    b = a + [ ]
+    assert(almostEqual(median(b), 2.5))
+    if (a != b):
+        raise Exception('Your median() function should be non-destructive!')
+    print('Passed!')
+
+def testSmallestDifference():
+    print('Testing smallestDifference()...', end='')
+    assert(smallestDifference([]) == -1)
+    assert(smallestDifference([5]) == -1)
+    assert(smallestDifference([2,3,5,9,9]) == 0)
+    assert(smallestDifference([-2,-5,7,15]) == 3)
+    assert(smallestDifference([19,2,83,6,27]) == 4)
+    assert(smallestDifference(list(range(0, 10**3, 5)) + [42]) == 2)
+    print('Passed!')
+
+def _verifyNondestructiveRemoveRepeatsIsNondestructive():
+    a = [3, 5, 3, 3, 6]
+    b = a + [ ] # copy.copy(a)
+    # ignore result, just checking for destructiveness here
+    nondestructiveRemoveRepeats(a)
+    return (a == b)
+
+def testNondestructiveRemoveRepeats():
+    print("Testing nondestructiveRemoveRepeats()", end="")
+    assert(_verifyNondestructiveRemoveRepeatsIsNondestructive())
+    assert(nondestructiveRemoveRepeats([1,3,5,3,3,2,1,7,5]) == [1,3,5,2,7])
+    assert(nondestructiveRemoveRepeats([1,2,3,-2]) == [1,2,3,-2])
+    assert(nondestructiveRemoveRepeats([]) == [])
+    print("Passed!")
+
+def testDestructiveRemoveRepeats():
+    print("Testing destructiveRemoveRepeats()", end="")
+    a = [1,3,5,3,3,2,1,7,5]
+    assert(destructiveRemoveRepeats(a) == None)
+    assert(a == [1,3,5,2,7])
+    b = [1,2,3,-2]
+    assert(destructiveRemoveRepeats(b) == None)
+    assert(b == [1,2,3,-2])
+    c = []
+    assert(destructiveRemoveRepeats(c) == None)
+    assert(c == [])
+    print("Passed!")
+
+def testIsSorted():
+    print('Testing isSorted()...', end='')
+    assert(isSorted([]) == True)
+    assert(isSorted([1]) == True)
+    assert(isSorted([1,1]) == True)
+    assert(isSorted([1,2]) == True)
+    assert(isSorted([2,1]) == True)
+    assert(isSorted([2,2,2,2,2,1,1,1,1,0]) == True)
+    assert(isSorted([1,1,1,1,2,2,2,2,3,3]) == True)
+    assert(isSorted([1,2,1]) == False)
+    assert(isSorted([1,1,2,1]) == False)
+    assert(isSorted(range(10,30,3)) == True)
+    assert(isSorted(range(30,10,-3)) == True)
+    print('Passed!')
+
+def _verifyRunLengthEncodeIsNondestructive():
+    a = [1,2,3]
+    b = a + [ ] # copy.copy(a)
+    runLengthEncode(a) # ignore result, just checking for destructiveness here
+    return (a == b)
+
+def testRunLengthEncode():
+    print("Testing runLengthEncode()...", end="")
+    assert(_verifyRunLengthEncodeIsNondestructive() == True)
+    assert(runLengthEncode([]) == [])
+    assert(runLengthEncode([1,1,1]) ==  [(3,1)])
+    assert(runLengthEncode([-1,2,7]) == [(1,-1),(1,2),(1,7)])
+    assert(runLengthEncode([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)])
+    assert(runLengthEncode([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)])
+    assert(runLengthEncode([2]*5 + [5]*2) == [(5,2), (2,5)])
+    assert(runLengthEncode([5]*2 + [2]*5) == [(2,5), (5,2)])
+    print("Passed!")
+
+def _verifyRunLengthDecodeIsNondestructive():
+    a = [(1,2), (2,3)]
+    b = a + [ ] # copy.copy(a)
+    runLengthDecode(a) # ignore result, just checking for destructiveness here
+    return (a == b)
+
+def testRunLengthDecode():
+    print("Testing runLengthDecode()...", end="")
+    assert(_verifyRunLengthDecodeIsNondestructive() == True)
+    assert(runLengthDecode([]) == [])
+    assert(runLengthDecode([(3,1)]) == [1,1,1])
+    assert(runLengthDecode([(1,-1),(1,2),(1,7)]) == [-1,2,7])
+    assert(runLengthDecode([(2,3),(1,8),(3,-10)]) == [3,3,8,-10,-10,-10])
+    assert(runLengthDecode([(5,2), (2,5)]) == [2]*5 + [5]*2)
+    assert(runLengthDecode([(2,5), (5,2)]) == [5]*2 + [2]*5)
+    print("Passed!")
+
+def testMultiplyPolynomials():
+    print("Testing multiplyPolynomials()...", end="")
+    # (2)*(3) == 6
+    assert(multiplyPolynomials([2], [3]) == [6])
+    # (2x-4)*(3x+5) == 6x^2 -2x - 20
+    assert(multiplyPolynomials([2,-4],[3,5]) == [6,-2,-20])
+    # (2x^2-4)*(3x^3+2x) == (6x^5-8x^3-8x)
+    assert(multiplyPolynomials([2,0,-4],[3,0,2,0]) == [6,0,-8,0,-8,0])
+    print("Passed!")
+
+def testBestScrabbleScore():
+    print("Testing bestScrabbleScore()...", end="")
+    def dictionary1(): return ["a", "b", "c"]
+    def letterScores1(): return [1] * 26
+    def dictionary2(): return ["xyz", "zxy", "zzy", "yy", "yx", "wow"] 
+    def letterScores2(): return [1+(i%5) for i in range(26)]
+    assert(bestScrabbleScore(dictionary1(), letterScores1(), list("b")) ==
+                                        ("b", 1))
+    assert(bestScrabbleScore(dictionary1(), letterScores1(), list("ace")) ==
+                                        (["a", "c"], 1))
+    assert(bestScrabbleScore(dictionary1(), letterScores1(), list("b")) ==
+                                        ("b", 1))
+    assert(bestScrabbleScore(dictionary1(), letterScores1(), list("z")) ==
+                                        None)
+    # x = 4, y = 5, z = 1
+    # ["xyz", "zxy", "zzy", "yy", "yx", "wow"]
+    #    10     10     7     10    9      -
+    assert(bestScrabbleScore(dictionary2(), letterScores2(), list("xyz")) ==
+                                         (["xyz", "zxy"], 10))
+    assert(bestScrabbleScore(dictionary2(), letterScores2(), list("xyzy")) ==
+                                        (["xyz", "zxy", "yy"], 10))
+    assert(bestScrabbleScore(dictionary2(), letterScores2(), list("xyq")) ==
+                                        ("yx", 9))
+    assert(bestScrabbleScore(dictionary2(), letterScores2(), list("yzz")) ==
+                                        ("zzy", 7))
+    assert(bestScrabbleScore(dictionary2(), letterScores2(), list("wxz")) ==
+                                        None)
+    print("Passed!")
+
+def relaxedAlmostEqual(d1, d2):
+    epsilon = 10**-3 # really loose here
+    return abs(d1 - d2) < epsilon
+
+def tuplesAlmostEqual(t1, t2):
+    if (len(t1) != len(t2)): return False
+    for i in range(len(t1)):
+        if (not relaxedAlmostEqual(t1[i], t2[i])):
+            return False
+    return True
+
+def testLinearRegression():
+    print("Testing bonus problem linearRegression()...", end="")
+
+    ans = linearRegression([(1,3), (2,5), (4,8)])
+    target = (1.6429, 1.5, .9972)
+    assert(tuplesAlmostEqual(ans, target))
+    
+    ans = linearRegression([(0,0), (1,2), (3,4)])
+    target = ((9.0/7), (2.0/7), .9819805061)
+    assert(tuplesAlmostEqual(ans, target))
+
+    #perfect lines
+    ans = linearRegression([(1,1), (2,2), (3,3)])
+    target = (1.0, 0.0, 1.0)
+    assert(tuplesAlmostEqual(ans, target))
+    
+    ans = linearRegression([(0,1), (-1, -1)])
+    target = (2.0, 1.0, 1.0)
+    assert(tuplesAlmostEqual(ans, target))
+
+    #horizontal lines
+    ans = linearRegression([(1,0), (2,0), (3,0)])
+    target = (0.0, 0.0, 1.0)
+    assert(tuplesAlmostEqual(ans, target))
+
+    ans = linearRegression([(1,1), (2,1), (-1,1)])
+    target = (0.0, 1.0, 1.0)
+    assert(tuplesAlmostEqual(ans, target))
+    print("Passed!")
+
+def testRunSimpleProgram():
+    print("Testing bonus problem runSimpleProgram()...", end="")
+    largest = """! largest: Returns max(A0, A1)
+                   L0 - A0 A1
+                   JMP+ L0 a0
+                   RTN A1
+                   a0:
+                   RTN A0"""
+    assert(runSimpleProgram(largest, [5, 6]) == 6)
+    assert(runSimpleProgram(largest, [6, 5]) == 6)
+
+    sumToN = """! SumToN: Returns 1 + ... + A0
+                ! L0 is a counter, L1 is the result
+                L0 0
+                L1 0
+                loop:
+                L2 - L0 A0
+                JMP0 L2 done
+                L0 + L0 1
+                L1 + L1 L0
+                JMP loop
+                done:
+                RTN L1"""
+    assert(runSimpleProgram(sumToN, [5]) == 1+2+3+4+5)
+    assert(runSimpleProgram(sumToN, [10]) == 10*11//2)
+    print("Passed!")
+
+#################################################
+# testAll and main
+#################################################
+
+def testAll():
+    # comment out the tests you do not wish to run!
+    testAlternatingSum()
+    testMedian()
+    testSmallestDifference()
+    testNondestructiveRemoveRepeats()
+    testDestructiveRemoveRepeats()
+    testIsSorted()
+    testRunLengthEncode()
+    testRunLengthDecode()
+    testMultiplyPolynomials()
+    testBestScrabbleScore()
+
+    # Bonus:
+    #testLinearRegression()
+    #testRunSimpleProgram() 
+
+def main():
+    cs112_f22_week4_linter.lint()
+    testAll()
+
+if __name__ == '__main__':
+    main()
